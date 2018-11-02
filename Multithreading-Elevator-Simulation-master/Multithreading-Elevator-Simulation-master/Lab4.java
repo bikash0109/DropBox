dd4deb3116cf1181bb12daca58551e0e500727ea
Bikash Roy <bikash0109@gmail.com>
2018-11-02 19:22:11 -0400

    ok

--- Multithreading-Elevator-Simulation-master/Multithreading-Elevator-Simulation-master/Lab4.java
+++
@@ -1,9 +0,0 @@
-// Wajih Yassine 42139560, Kevin Yin 29211757
-public class Lab4 {
-	//Main method that starts the ElevatorSimulation 
-	public static void main(String[] args) {
-		ElevatorSimulation ElevatorSim = new ElevatorSimulation();
-		ElevatorSim.start();
-	}
-
-}
